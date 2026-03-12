71a103421a933ef1e681bf74ee5fa38857467368
jwsbreal <j.wsbreal@gmail.com>
2026-03-12 12:52:55 -0300

    Atualizado projeto utilizando fixtures, DDT e commands

--- projects/testes_automatizados/saude-demo-ddt/cypress/e2e/saude-demo-ddt-spec.cy.js
+++ projects/testes_automatizados/saude-demo-ddt/cypress/e2e/saude-demo-ddt-spec.cy.js
@@ -1,18 +1,29 @@
 import loginPage from "../pages/loginPage";
 
 describe("Sauce Demo DDT - Login", () => {
-
-  beforeEach((), => {
-    cy.loginPage();
-
+  beforeEach(() => {
+    cy.loginPage(); //login feito através de POM, utilizando a loginPage.js
   });
-  describe("Login com usuários disponíveis", () => {
 
-      it("Login Standar", () => {
-       cy.loginStandar();
-      });
+  before(function () {
+    cy.fixture("users").as("userData"); //carregamento da fixture users.json e atribuição a um alias "userData" posterior
+  });
+
+  before(function () {
+    const users = this.userData;
+  });
+
+  Object.keys(users).forEach((userKey) => {
+    it(`Deve realizar login com o usuário: ${userKey}`, function () {
+      cy.loginPage();
+      cy.login(userKey); //login feito através de comando customizado, utilizando os dados do fixture users.json
+
+      if (userKey === "errorUser") { 
+        cy.get('[data-test="error"]').should("exist");
+      } else {
+        cy.url().should("include", "inventory");
+      }  //verificação específica para o usuário "errorUser", que é esperado que apresente uma mensagem de erro
 
     });
-
-
+  });
 });
--- projects/testes_automatizados/saude-demo-ddt/cypress/support/commands.js
+++ projects/testes_automatizados/saude-demo-ddt/cypress/support/commands.js
@@ -1,9 +1,9 @@
-Cypress.Commands.add("loginPadrao", () => {
-  cy.fixture("users").then((user) => {
-    cy.visit("https://www.saucedemo.com/");
+Cypress.Commands.add("login", (userKey) => {
+  cy.fixture("users").then((users) => {
+    const user = users[userKey];
 
-    cy.get("#user-name").type(user.standardUser.username);
-    cy.get("#password").type(user.standardUser.password);
+    cy.get("#user-name").type(user.username);
+    cy.get("#password").type(user.password);
     cy.get("#login-button").click();
 
     cy.url().should("include", "/inventory.html");
